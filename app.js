793e0a6452d3cb6b5861ea1670ca6e2a0ed16e11
VukashinO <vukashin@hotmail.com>
2020-10-25 22:26:02 +0100

    removed comments

--- app.js
+++ app.js
@@ -5,22 +5,10 @@ const app = express();
 
 app.use(express.json());
 
-// HC array
-// const courses = [
-//     {id: 1, name: 'JS'}, 
-//     {id:2, name: 'C#'},
-// ];
-
 app.get('/', (req, res) => {
     res.send('Hello World!!!!')
 });
 
-// with HC array
-// app.get('/api/courses', (req, res) => {
-//     res.send(courses);
-// });
-
-// static FS
 app.get('/api/courses', (req, res) => {
     fs.readFile('courses.json', 'utf-8', (err, data) => {
         if(err) return err;
@@ -29,16 +17,6 @@ app.get('/api/courses', (req, res) => {
         return res.send(courses);
     })
 });
-
-// With HC arr
-// app.get('/api/courses/:id', (req, res) => {
-//     const course = courses.find(c => c.id === parseInt(req.params.id));
-//     // 404 bad request object not found by convection
-//     if(!course) return res.status(404).send('The course with the given Id was not found!');
-//     return res.send(course);
-// });
-
-// Static FS
 
 app.get('/api/courses/:id', (req, res) => {
     fs.readFile('courses.json', 'utf-8', (err, data) => {
@@ -52,19 +30,6 @@ app.get('/api/courses/:id', (req, res) => {
     })
 });
 
-// WITH HC ARRAY
-// app.post('/api/courses', (req, res) => {
-//     const { error } = validateCourse(req.body.name );
-//     if (error) return res.status(400).send(error.message);
-//     const course = {
-//         id: courses.length + 1,
-//         name: value
-//     }
-//     courses.push(course);
-//     return res.send(course);
-// });
-
-// with static FS
 app.post('/api/courses', (req, res) => {
     const { error } = validateCourse(req.body);
     if (error) return res.status(400).send(error.message);
@@ -85,30 +50,6 @@ app.post('/api/courses', (req, res) => {
     })
 });
 
-// HS ARR
-// app.put('/api/courses/:id', (res, req) => {
-//     const course = courses.find(c => c.id === +req.params.id);
-//     if(!course) return res.status(404).send('The course with the given Id was not found!');
-
-//     const { error } = validateCourse(request.body);
-//     if (error) return res.status(400).send(error.message);
-
-//     course.name = req.body.name;
-//     res.send(course);
-// });
-
-// Static FS
-
-// app.put('/api/courses/:id', (res, req) => {
-//     const course = courses.find(c => c.id === +req.params.id);
-//     if(!course) return res.status(404).send('The course with the given Id was not found!');
-
-//     const { error } = validateCourse(request.body);
-//     if (error) return res.status(400).send(error.message);
-
-//     course.name = req.body.name;
-//     res.send(course);
-// });
 app.put('/api/courses/:id', (req, res) => {
     const { error } = validateCourse(req.body);
     if (error) return res.status(400).send(error.message);
@@ -126,18 +67,6 @@ app.put('/api/courses/:id', (req, res) => {
         return res.send(course);
     })
 });
-
-// HC ARRAY
-// app.delete('/api/courses/:id', (req, res) => {
-//     const course = courses.find(c => c.id === +req.params.id);
-//     if(!course) return res.status(404).send('The course with the given Id was not found!');
-//     const index = courses.indexOf(course);
-//     courses.splice(index, 1);
-
-//     res.send(course);
-// });
-
-// FS STATIC
 
 app.delete('/api/courses/:id', (req, res) => {
     fs.readFile('courses.json', 'utf-8', (err, data) => {
